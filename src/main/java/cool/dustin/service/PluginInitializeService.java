2e5879156b90274e7f7250f6cbcc40b20f130b61
Dustin <gjf66924011@gmail.com>
2020-04-28 16:15:36 +0800

    没有配置文件的时候不解析配置文件

--- src/main/java/cool/dustin/service/PluginInitializeService.java
+++ src/main/java/cool/dustin/service/PluginInitializeService.java
@@ -7,6 +7,7 @@ import cool.dustin.constant.MessageDefine;
 import cool.dustin.datas.PluginRuntimeData;
 import cool.dustin.model.PluginSetting;
 import cool.dustin.util.MessageUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -22,6 +23,10 @@ public class PluginInitializeService implements StartupActivity {
         PluginSetting setting = instance.getState();
         if (setting == null) {
             MessageUtils.showMessage(MessageDefine.NO_SETTING);
+            return;
+        }
+
+        if (StringUtils.isEmpty(setting.getTemplateXmlPath())) {
             return;
         }
 
